7014aeffa87b86878bed343debab773af52023d5
ASTeteria <teteryawushu@gmail.com>
2025-01-01 17:08:37 +0200

    Init

---
+++ src/main/java/Spring_HW/SpringBoot.java
@@ -0,0 +1,11 @@
+package Spring_HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class, args);
+    }
+}
