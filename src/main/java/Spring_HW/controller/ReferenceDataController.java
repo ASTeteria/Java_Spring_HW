baf7ac95e3b86f82aa0b3eacc5f7ef03f19dee7b
ASTeteria <teteryawushu@gmail.com>
2025-01-01 23:09:22 +0200

    HW1

--- src/main/java/Spring_HW/controller/ReferenceDataController.java
+++ src/main/java/Spring_HW/controller/ReferenceDataController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/cars")
 @RequiredArgsConstructor
 public class ReferenceDataController {
 
